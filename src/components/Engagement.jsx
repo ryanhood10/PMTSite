e9099ebd378be994276a289c2f7a2f9beace030a
Ryan Hood <ryanhood@Ryans-MacBook-Pro.local>
2023-11-21 19:02:37 -0600

    styled Engagement

--- src/components/Engagement.jsx
+++ src/components/Engagement.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import DumpsterImage from '../PMTphotos/alphabet-word-images-1293460_1920.png';
+import stock2 from '../PMTphotos/dialogue-window-148174_1280.png'
 import NashMap from '../PMTphotos/NashMap1.png';
 import { useNavigate } from 'react-router-dom';
 
@@ -43,79 +44,119 @@ const DumpsterRentalCompany = () => {
 
     return (
         <div className="w-full  bg-white py-16 px-4">
-            <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
-            <div className={`animation-container ${animationClasses(isVisible)}`}>
-                    <img className="max-h-[400px]  rounded-3xl" src={DumpsterImage} alt="Dumpster" />
-                </div>
 
-      
+            {/* first column */}
+            <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 pb-2">
 
                 <div className={`animation-container ${animationClasses(isVisible)}`}>
-                    <p className="text-green-700 text-xl font-bold">Nashville Dumpster Rentals</p>
-                    <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">Your Trusted Dumpster Rental Solution</h1>
-                    <p className="text-xl"> 
-                        Nashville Dumpster Rentals specializes in providing high-quality dumpster containers primarily for construction sites. We offer a wide range of container sizes to meet your project's needs, along with reliable delivery and pickup services.
-                    </p>
-                    <p className="text-green-600 text-lg">Contact us today to discuss your dumpster rental requirements.</p>
-                    <button onClick={handleContactUs} className="bg-green-400 w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white hover:bg-green-600">Contact Us</button>
+          <h1 className="text-[#84BD00] text-xl font-bold">PMT Site</h1>
+          <h2 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+            Your Expert Subcontracting Partner in Hermitage, TN
+          </h2>
+          <p className="text-xl">
+            PMT Site is a leading subcontracting company in Hermitage,
+            TN, founded in 2019. We specialize in providing high-quality
+            subcontracting services to a wide range of clients in the greater
+            Nashville area. Our experienced team is committed to delivering
+            exceptional results on every project we undertake.
+          </p>
+          <p className="text-[#84BD00] text-lg">
+            Contact us today to learn more about how we can help you with your
+            project.
+          </p>
+          <button
+            onClick={handleContactUs}
+            className="bg-[#84BD00] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white active:bg-[#001489] hover:bg-[#c1e867]"
+          >
+            Contact Us
+          </button>
+        </div>
+
+        <div className={`animation-container ${animationClasses(isVisible)}`}>
+                    <img className="max-h-[400px]  rounded-3xl" src={stock2} alt="Dumpster" />
+                    
                 </div>
 
               
             </div>
 
-            <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
-                <div className={`animation-container ${animationClasses(isVisible)}`}>
-                    <h1 className="text-center md:text-4xl sm:text-3xl text-2xl font-bold py-2">Our Dumpster Services</h1>
-                    <p className="text-center md:text-xl sm:text-xl text-xl justify-center  font-semibold">
-                        Discover our comprehensive dumpster rental services:
-                    </p>
-                    <div className="flex flex-wrap justify-center py-4">
-                        <ul className="font-semibold text-2xl grid grid-cols-2 md:grid-cols-3 gap-y-4 gap-x-8 text-green-700">
-                            <li className="flex items-center">
-                                <span className="mr-2">Construction Site Rentals</span>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="mr-2">Various Container Sizes</span>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="mr-2">Flexible Rental Periods</span>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="mr-2">Prompt Delivery & Pickup</span>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="mr-2">Affordable Pricing</span>
-                            </li>
-                            <li className="flex items-center">
-                                <span className="mr-2">Dedicated Customer Support</span>
-                            </li>
+                        {/* second column */}
+            <div className="max-w-[1240px] mx-auto pt-2 pb-28 space-x-4 grid md:grid-cols-2">
+
+            <div className={`animation-container  ${animationClasses(isVisible)}`}>
+                    
+                    <div className="flex space-y-2">
+                        <img className="w-full rounded-3xl" src={NashMap} alt="Dumpster" />
+                    </div>
+                    <h4 className="text-center md:text-xl sm:text-xl text-xl justify-center font-semibold">PMT Subcontracting: Providing exceptional subcontracting services to the greater Nashville area, built on trust, integrity, and a commitment to quality.</h4>
+
+                </div>
+
+        <div className={`animation-container ${animationClasses(isVisible)}`}>
+          <h2 className="text-center md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+            Our Comprehensive Subcontracting Services
+          </h2>
+          <p className="text-center md:text-xl sm:text-xl text-xl justify-center font-semibold">
+            PMT Subcontracting offers a comprehensive range of subcontracting
+            services to meet the diverse needs of our clients, including:
+          </p>
+          <div className="flex flex-wrap justify-center py-4">
+            <ul className="font-semibold text-2xl grid grid-cols-2 md:grid-cols-3 gap-y-4 gap-x-8 text-green-700">
+              <li className="flex items-center">
+                <span className="mr-2">Excavation and Site Preparation</span>
+              </li>
+              <li className="flex items-center">
+                <span className="mr-2">Concrete Foundations and Slabs</span>
+              </li>
+              <li className="flex items-center">
+                <span className="mr-2">Framing and Carpentry</span>
+              </li>
+              <li className="flex items-center">
+                <span className="mr-2">Roofing and Siding</span>
+              </li>
+              <li className="flex items-center">
+                <span className="mr-2">Finishing and Interior Trim</span>
+              </li>
+              <li className="flex items-center">
+                <span className="mr-2">Mechanical and Electrical Systems</span>
+              </li>
+              <li className="flex items-center">
+                <span className="mr-2">Custom Millwork and Cabinetry</span>
+              </li>
+              <li className="flex items-center">
+                <span className="mr-2">Site Development and Landscaping</span>
+              </li>
                             
                         </ul>
                     </div>
                 </div>
 
-                <div className={`animation-container ${animationClasses(isVisible)}`}>
-                    <div className="flex items-center justify-center h-full">
-                        <img className="w-full rounded-3xl" src={NashMap} alt="Dumpster" />
-                    </div>
-                    <p className="text-center md:text-xl sm:text-xl text-xl justify-center  font-semibold"> See about dumpster container rentals in the greater Nashville area
-                    </p>
-                </div>
+                
 
             </div>
 
-            <div className="max-w-[1240px] mx-auto pt-24 px-8">
-                <p className="text-center text-2xl font-semibold pb-6">Why Choose Nashville Dumpster Rentals</p>
-                <div className="flex flex-col justify-center text-lg">
-                    <p className="text-lg font-bold">At Nashville Dumpster Rentals, we take pride in our commitment to serving construction sites with reliable and efficient dumpster rental solutions. Here's why you should choose us:</p>
-                    <ul className="list-disc list-inside pl-6 mt-2">
-                        <li className="mb-2">Experience and Expertise in Construction Site Rentals</li>
-                        <li className="mb-2">Wide Range of Container Sizes to Fit Your Needs</li>
-                        <li className="mb-2">Flexible Rental Options to Suit Your Schedule</li>
-                        <li className="mb-2">Timely Delivery and Pickup Services</li>
-                        <li className="mb-2">Competitive and Transparent Pricing</li>
-                    </ul>
-                </div>
+                                    {/* 3rd column */}
+            <div className="max-w-[1240px] mx-auto grid md:grid-cols-5 pb-2">
+            <img className="max-h-[400px] rounded-3xl" src={DumpsterImage} alt="Dumpster" />
+
+            <div className={`animation-container col-span-4 flex ${animationClasses(isVisible)}`}>
+                
+
+                    <div class="col-span-2">
+                    <p class="text-2xl font-semibold pb-6">"We're Not Just Stepping Up to the Plate, We're Climbing the Ladder"</p>
+                        <div class="flex flex-col justify-center text-lg">
+                            <p class="text-lg font-bold">Why choose PMT site for your subcontracting job?</p>
+                            <ul class="list-disc list-inside pl-6 mt-2">
+                            <li class="mb-2">Unparalleled Experience and Expertise</li>
+                            <li class="mb-2">Comprehensive Subcontracting Solutions</li>
+                            <li class="mb-2">Unwavering Commitment to Safety</li>
+                            <li class="mb-2">Dedicated Customer Service</li>
+                            <li class="mb-2">Competitive and Transparent Pricing</li>
+                            </ul>
+                        </div>
+                    </div>
+            </div>
+
             </div>
 
          
--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -1,64 +1,50 @@
 import React from 'react';
-import stock1 from '../PMTphotos/subcontractorStock-transformed.png'
-import stock2 from '../PMTphotos/subcontractStock2-transformed.jpeg'
-import stock3 from '../PMTphotos/subcontractorStock3.jpeg'
-import { useNavigate } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa'; // Importing a user icon from react-icons
 
+const ContactCards = () => {
+   
 
-const HeadlineCards = () => {
+    const contacts = [
+      {
+        name: "Phil Terhaar",
+        title: "Owner/President",
+        phone: "618-402-7440",
+        email: "pterhaar@pmtsite.com"
+      },
+      {
+        name: "Dominic Charielle",
+        title: "Owner/COO",
+        phone: "630-636-0838",
+        email: "dcharielle@pmtsite.com"
+      },
+      {
+        name: "Brett Alfrey",
+        title: "CFA",
+        phone: "859-468-5684",
+        email: "balfrey@pmtsite.com"
+      }
+    ];
 
-    const navigate = useNavigate();
-
-    const handleViewTestimonials = () => {
-      navigate('/testimonials');
-    };
-
-    
-  const scrollToRobots = () => {
-    // Scroll to the target element in a different component
-    const targetElement = document.getElementById('robotsComponent');
-    if (targetElement) {
-      targetElement.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
-
-  return (
-    <div className='mx-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
-      {/* Card 1 */}
-      <div className='rounded-xl relative hover:scale-105 duration-300'>
-        {/* Overlay */}
-        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-          <p className='font-bold text-2xl px-2 pt-4'>See Customer</p>
-          <p className='px-2'>Testimonials</p>
-          <button href='/Testimonials' className='border-white bg-white text-black mx-2 absolute bottom-4' onClick={handleViewTestimonials}>View Now</button>
-        </div>
-        <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src={stock1} alt='robot2' />
+    return (
+      <div>
+      <h1 className='text-center text-3xl font-semibold text-[#001489]'> Contact Information </h1>
+      <div className='mx-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+        {contacts.map((contact, index) => (
+          <div key={index} className='rounded-xl relative hover:scale-105 duration-300 border-2 border-black shadow-md bg-gray-200'>
+            <div className='absolute top-4 right-4'>
+              <FaUserCircle size={50} /> {/* Profile Icon */}
+            </div>
+            <div className='p-4 text-black'>
+              <h3 className='font-bold text-xl'>{contact.name}</h3>
+              <p className='text-md'>{contact.title}</p>
+              <p className='text-md'>{contact.phone}</p>
+              <a href={`mailto:${contact.email}`} className='text-md text-blue-600 font-semibold hover:font-bold '>{contact.email}</a>
+            </div>
+          </div>
+        ))}
       </div>
-
-      {/* Card 2 */}
-      <div className='rounded-xl relative hover:scale-105 duration-300'>
-        {/* Overlay */}
-        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-          <p className='font-bold text-2xl px-2 pt-4'>Premium Robots</p>
-          <p className='px-2'>Great Prices</p>
-          <button onClick={scrollToRobots} className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
-        </div>
-        <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src={stock2} alt='robot2' />
       </div>
-
-      {/* Card 3 */}
-      <div className='rounded-xl relative hover:scale-105 duration-300'>
-        {/* Overlay */}
-        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-          <p className='font-bold text-2xl px-2 pt-4'>Premium Software</p>
-          <p className='px-2'>For Functional Robots</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
-        </div>
-        <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src={stock3} alt='robot2' />
-      </div>
-    </div>
-  );
+    );
 }
 
-export default HeadlineCards;
+export default ContactCards;
